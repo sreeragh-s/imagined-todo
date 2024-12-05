4677613c5a96fc94efe2deb34086dba05e913ec9
sreeragh <sreeragh@bizmo.site>
2024-12-05 15:21:25 +0530

    fixed unused vars error during deployment

--- app/components/todo/add-todo-dialog.tsx
+++ app/components/todo/add-todo-dialog.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import { ResponsiveModal, ResponsiveModalTrigger, ResponsiveModalClose, ResponsiveModalPortal, ResponsiveModalOverlay, ResponsiveModalContent, ResponsiveModalHeader, ResponsiveModalTitle } from "@/components/ui/responsice-modal";
+import { ResponsiveModal, ResponsiveModalTrigger, ResponsiveModalContent, ResponsiveModalHeader, ResponsiveModalTitle } from "@/components/ui/responsice-modal";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Plus, CalendarIcon } from "lucide-react";
